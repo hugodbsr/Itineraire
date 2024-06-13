5d51c5ef58f19fd5902cf330ae299c611400ffa0
hugo <hugo.debuyser.etu@univ-lille.fr>
2024-06-13 18:23:42 +0200

    Addition IHM

---
+++ src/IHM/sae/FXMLdemo.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FXMLdemo extends Application {
+
+    public void start(Stage stage) throws IOException {
+            FXMLLoader loader = new FXMLLoader();
+            URL fxmlFileUrl = getClass().getResource("Main.fxml");
+            if (fxmlFileUrl == null) {
+                    System.out.println("Impossible de charger le fichier fxml");
+                    System.exit(-1);
+            }
+            loader.setLocation(fxmlFileUrl);
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("FXML demo");
+            stage.show();
+    }
+
+    public static void main(String[] args) {
+            Application.launch(args);
+    }
+}
