3bd52e64cdf4b7aac1e361b99ae52e566edbbf6e
Gaël Dierynck <gaeldierynck@gmail.com>
2024-06-17 20:53:23 +0200

    IHM-V2 On peut maintenant changer de pane(Need fix)

--- src/IHM/MonController.java
+++ src/IHM/MonController.java
@@ -3,10 +3,11 @@ import Poo.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.stackPane;
+import javafx.scene.layout.StackPane;
 public class MonController {
     @FXML
     TextField tempsField;
@@ -20,11 +21,36 @@ public class MonController {
      @FXML
      CheckBox checkTrain;
 
+     @FXML
+     AnchorPane renduApane;
 
-        private void switchPane(Pane pane) {
-                stackPane.getChildren().forEach(child -> child.setVisible(false));
-                pane.setVisible(true);
+     @FXML
+     AnchorPane rechercherApane;
+
+     @FXML
+     AnchorPane choixApane;
+
+     @FXML
+     StackPane stackPane; 
+
+     @FXML
+     AnchorPane anchorPane;
+     
+       @FXML
+        Button finaliserButton;
+
+        @FXML
+        Button menuButton;
+
+        @FXML
+        Button saveExitButton;
+
+
+    private void switchPane(AnchorPane pane) {
+        stackPane.getChildren().forEach(child -> child.setVisible(false));
+        pane.setVisible(true);
     }
+
     public void initialize() {
             System.out.println("Initialisation...");
             histoButton.setOnAction(event -> {
@@ -32,10 +58,36 @@ public class MonController {
             });
     }
 
-    public void pressedButtonPlus(ActionEvent event) {
+
+    public void pressedButtonHisto() {
+        menuButton.setOnAction(event -> {
+                switchPane(choixApane);
+                });
+    }
+
+    public void pressedButtonRechercher() {
+        rechercherButton.setOnAction(event -> {
+                switchPane(rechercherApane);
+                });
+    }
+
+    public void pressedButtonStop() {
+        saveExitButton.setOnAction(event -> {
+                System.exit(0);
+        //TODO : appeller la fonction save de la classe Historique        
+        });
+    }
+
+    public void pressedButtonFinaliser() {
+        finaliserButton.setOnAction(event-> {
+            switchPane(renduApane);
+        });
+    }
+    //Fonction de test
+    /*public void pressedButtonPlus(ActionEvent event) {
             int newValue = Integer.parseInt(tempsField.getText()) + 1;
             tempsField.setText("" + newValue);
-    }
+    }*/
 
 }
 
